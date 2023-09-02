f640db8bd49cb50714d5794e104dcf06d8c8289c
Hồ Lê Giỏi <legioiho@gmail.com>
2023-09-02 10:25:03 +0700

    Add

--- CodeTodoList/app.js
+++ CodeTodoList/app.js
@@ -8,12 +8,55 @@ form.addEventListener('submit', function(event){
     let val = input.value.trim()
 
     if(val){
-
+        addTodoElement({
+            text: val,
+        })
     }
+
+    input.value = ''
 })
 
 function addTodoElement(todo){
     var li = document.createAttribute('li')
     li.innerHTML =
-     ' <span>${}</span> <i class="far fa-trash-alt"></i>'
+     `<span>${todo.text}</span>
+     <i class="far fa-trash-alt"></i>`
+
+    if(todo.status === 'compited'){
+        li.setAttribute('class', 'completed')
+    }
+
+    li.addEventListener('click', function(){
+        this.classList.toggle('completed')
+    })
+
+    li.querySelector('i').addEventListener('click', function(){
+        this.parentElement.remove()
+    })
+
+    todos.appendChild(li)
 }
+
+function saveTodoList(){
+    let todoList = document.querySelectorAll('li')
+    let todoStrorage = []
+    todoList.forEach(function(item){
+        let text = item.querySelector('span').innerText
+        let status = item.getAttribute('class')
+        todoStrorage.push({
+            text,
+            status
+        })
+    })
+
+    todoStrorage.setItem('todoList', JSON.stringify(todoStrorage))
+}
+
+function init(){
+    let data = JSON.parse(localStorage.getItem('todoList'))
+    data.forEach(function(item){
+        addTodoElement(item)
+    })
+}
+
+init()
