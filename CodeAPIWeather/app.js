20584f3d087bca064c62ef2a897e147715cf41e6
Hồ Lê Giỏi <legioiho@gmail.com>
2023-09-03 09:35:55 +0700

    Add code

--- CodeAPIWeather/app.js
+++ CodeAPIWeather/app.js
@@ -7,14 +7,50 @@ var visibility = document.querySelector('.visibility')
 var wind = document.querySelector('.wind')
 var sun = document.querySelector('.sun')
 var search = document.querySelector('.search')
-
+var value = document.querySelector('.value')
+var time = document.querySelector('.time')
+var content = document.querySelector('.content')
+var body = document.querySelector('body')
 
 async function changeWeather(){
-    search.value.trim()
+    let capitalSearch = search.value.trim()
 
-    let apiULR = 'https://api.openweathermap.org/data/2.5/weather?q=ha noi&appid=bf2af3ddf146cdd744c49932b34014af'
+    let apiULR = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=bf2af3ddf146cdd744c49932b34014af`
 
     let data = await fetch(apiULR).then(res=> res.json())
-    console.log(data)
+    if(data.cod == 200)
+    {
+        content.classList.remove('hide')
+        city.innerText = data.name
+        country.innerText = data.sys.country
+        visibility.innerText = data.visibility + ' (m)'
+        wind.innerText = data.wind.speed + ' (m/s)'
+        sun.innerText = data.main.humidity + ' (%)'
+        let temp = Math.round((data.main.temp -273.15))
+        value.innerText = temp
+        shortDesc.innerText = data.weather[0] ? data.weather[0].main : ''
+        time.innerText = new Date().toLocaleString('vi')
 
+        body.setAttribute('class', 'warm')
+        if(temp <= 25){
+            body.setAttribute('class', 'cool')
+        }
+
+        if(temp <= 22){
+            body.setAttribute('class', 'warm')
+        }
+
+        if(temp <= 19){
+            body.setAttribute('class', 'cold')
+        }
+
+    }else{
+        content.classList.add('hide')
+    }
 }
+
+search.addEventListener('keypress', function(e){
+    if(e.code == 'Enter'){
+        changeWeather()
+    }
+})
