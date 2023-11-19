3eb97d20ddf33390aee54d319dac07615c801e88
Hồ Lê Giỏi <legioiho@gmail.com>
2023-11-19 10:41:51 +0700

    Add code dark light mode

---
+++ DarkLightMode/app1.js
@@ -0,0 +1,15 @@
+var swBtn = document.querySelector('label');
+var body = document.querySelector('body');
+
+function init(){
+    let mode = localStorage.getItem('mode') ? "dark" : "";
+    body.setAttribute('class', mode);
+}
+
+init()
+
+swBtn.addEventListener('click', ()=>{
+    body.classList.toggle('dark');
+    let mode = body.getAttribute('class') ? "dark" : "";
+    localStorage.setItem('mode', mode);
+})
